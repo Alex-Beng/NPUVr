246dd5343a62ab87dc464816bc7be55ce6e8a920
Alex Beng <599267065@qq.com>
2020-10-28 00:46:17 +0800

    update hand_demo keypoint

--- Pose/hand_demo.cpp
+++ Pose/hand_demo.cpp
@@ -1,9 +1,18 @@
+// run like
+// ./Pose/hand_demo /media/alex/Data/baseRelate/code/learnopencv/HandPose/hand/pose_deploy.prototxt /media/alex/Data/baseRelate/code/learnopencv/HandPose/hand/pose_iter_102000.caffemodel
+
 #include "hand_pose.h"
 
 string protoFile = "hand/pose_deploy.prototxt";
 string weightsFile = "hand/pose_iter_102000.caffemodel";
 
 int main(int argc, char **argv) {
+    // arg parser
+    cv::CommandLineParser parser(argc, argv,
+        "{@arg1||}{@arg2||}");
+    protoFile = samples::findFile(parser.get<std::string>(0));
+    weightsFile = samples::findFile(parser.get<std::string>(1));
+
 
     cv::VideoCapture cap(0);
     if (!cap.isOpened()) {
@@ -22,8 +31,14 @@ int main(int argc, char **argv) {
         cap >> frame;
         std::vector<Point> res_points;
         hp.ImageProc(frame, res_points, res_frame);
-        
-        // imshow("Output-Keypoints", frameCopy);
+
+        Mat frameCopy = frame.clone();
+
+        for (auto pnt : res_points) {
+            cv::circle(frameCopy, pnt, 8, cv::Scalar(0, 255, 255), -1);
+        }
+
+        imshow("Output-Keypoints", frameCopy);
         imshow("Output-Skeleton", res_frame);
         
         char key = waitKey(1);
